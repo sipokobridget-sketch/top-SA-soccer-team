8cbfc0c15a800fef81f3babfc12d0abdea74f2a7
sipokobridget-sketch <sipokobridget@gmail.com>
2026-04-09 16:07:50 +0200

    added arrays and string

--- app/src/main/java/com/example/topsasoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsasoccerteams/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.topsasoccerteams
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Arrays
 
 class MainActivity : AppCompatActivity() {
 
-    val teams = arrayOf<String>("Man Sundown", "Man City", "Man United", "Tottenham")
-    
+    val teams = arrayOf<String>("Man Sundown FC", "Man City", "Man United", "Tottenham")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val teamsTxt = findViewById<TextView>(R.id.textView)
+
+        //u
+        teams[0] = "Man Sundown FC :)"
+        //display top 5 soccer teams in the textview on UI
+        teamsTxt.text = Arrays.toString(a = teams)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
