827b8234e9b937bc2a340cd16a1773d987213ac6
sipokobridget-sketch <sipokobridget@gmail.com>
2026-04-09 15:58:11 +0200

    added pictures and created arrays

--- app/src/main/java/com/example/topsasoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsasoccerteams/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val teams = arrayOf<String>("Man Sundown", "Man City", "Man United", "Tottenham")
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
